1585936ed728e48aea093436f44591350795b4f5
Rebekah Zhou <rebekahzhou@gmail.com>
2022-04-06 13:24:00 -0700

    start adding standards to seed file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :course
+
+  has_many :domains
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+puts 'seeding! 🌱 🌱 🌱 '
+
+
+file = File.read('client/public/standards.json')
+data_hash = JSON.parse(file)
+
+data_hash['data']['standards'].map do |std, v| 
+    case v['statementLabel']
+    when "Course"
+        Course.create(name: v["description"]) 
+    when "Conceptual Category"
+        puts 'yo yo yo'
+    end
+end
+
+
+
+
+puts 'done seeding! 🌱 🌱 🌱'
